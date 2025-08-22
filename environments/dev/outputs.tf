34aa0402324a592e7749ee410f5125294977fb74
Danilo Casabona <danilocasabona>
2025-08-22 19:21:49 -0300

    fix(infra): separar outputs do bootstrap e da rede (vpc/subnets)

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,1 +1,12 @@
 output "region" { value = var.aws_region }
+output "vpc_id" {
+  value = aws_vpc.gb.id
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
