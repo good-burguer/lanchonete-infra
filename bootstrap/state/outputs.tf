f2617cec9a07bde8b1507b3fb510bcdf97506350
Danilo Casabona <danilocasabona>
2025-08-22 19:16:16 -0300

    chore(infra): padronizar tags e outputs para Good-Burger

--- bootstrap/state/outputs.tf
+++ bootstrap/state/outputs.tf
@@ -1,2 +1,14 @@
 output "bucket"     { value = aws_s3_bucket.tf_state.bucket }
 output "lock_table" { value = aws_dynamodb_table.tf_lock.name }
+
+output "vpc_id" {
+  value = aws_vpc.gb.id
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
