1363d2e0f6041ac1e04bddac71eab7619217ec08
Danilo Casabona <danilocasabona>
2025-08-23 20:09:06 -0300

    chore(ci): adicionar passo de debug para validar variáveis no workflow

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,9 @@
 variable "aws_region" { type = string }
 variable "tf_state_bucket" { type = string }
 variable "tf_lock_table" { type = string }
+variable "eks_cluster_name"   { type = string, default = "gb-dev-eks" }
+variable "eks_version"        { type = string, default = "1.29" } # não fixe patch
+variable "eks_min_size"       { type = number, default = 1 }
+variable "eks_max_size"       { type = number, default = 2 }
+variable "eks_desired_size"   { type = number, default = 1 }
+variable "eks_instance_types" { type = list(string), default = ["t3.small"] }
